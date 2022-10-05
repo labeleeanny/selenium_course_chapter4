2e18c85cd4dfb5a55910dfa817447b1ae19a93b5
labeleeanny <https://github.com/labeleeanny>
2022-10-05 11:40:18 +0300

    remove mark from test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -7,13 +7,8 @@ from .pages.product_page import ProductPage
 
 
 @pytest.mark.need_review
-@pytest.mark.parametrize('links',
-                         ["?promo=offer0", "?promo=offer1", "?promo=offer2", "?promo=offer3", "?promo=offer4",
-                          "?promo=offer5", "?promo=offer6",
-                          pytest.param("?promo=offer7", marks=pytest.mark.xfail), "?promo=offer8",
-                          "?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, links):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{links}"
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_product_link()
